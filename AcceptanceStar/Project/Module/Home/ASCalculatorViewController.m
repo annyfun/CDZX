aa41317f148a338b7fde62bd0ec7aaf0b90e02bb
Hemy <thislifee@qq.com>
2016-09-25 15:03:11 +0800

    aaa

--- AcceptanceStar/Project/Module/Home/ASCalculatorViewController.m
+++ AcceptanceStar/Project/Module/Home/ASCalculatorViewController.m
@@ -9,23 +9,39 @@
 #import "ASCalculatorViewController.h"
 #import "CaculatorUtility.h"
 
+#define kTopHeight  (64+108)
+#define kPadHeight  (SCREEN_HEIGHT-kTopHeight)
+#define kPadTopHeight 35
+
 @interface ASCalculatorViewController ()
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *displayViewHeight;
-@property (strong, nonatomic) IBOutletCollection(NSLayoutConstraint) NSArray *btnViewHeights;//100 / 120
-@property (strong, nonatomic) IBOutletCollection(NSLayoutConstraint) NSArray *btnViewTops;//10 / 20
-
-@property (weak, nonatomic) IBOutlet UILabel *display;
-@property (assign, nonatomic) BOOL isNew;
-
+    
+    @property (weak, nonatomic) IBOutlet UILabel *display;
+    @property (assign, nonatomic) BOOL isNew;
+    
+    @property (assign, nonatomic) BOOL isPadHidden;
+    
+    @property (weak, nonatomic) IBOutlet NSLayoutConstraint *btnHeight;
+    
+    @property (weak, nonatomic) IBOutlet NSLayoutConstraint *btnViewHeight;
+    @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomC;
 @end
 
 @implementation ASCalculatorViewController
+- (IBAction)hiddenPress:(id)sender {
+    
+    self.isPadHidden = !self.isPadHidden;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        CGFloat bottom = self.isPadHidden?-(self.btnViewHeight.constant-self.btnHeight.constant):0;
+        self.bottomC.constant = bottom;
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        
+    }];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
-    return;
     
     self.isNew = YES;
     self.display.text = self.params[kParamNumber];
@@ -33,15 +49,10 @@
         self.display.text = @"0";
     }
     self.navigationItem.title = @"银行承兑贴现计算器";
-    if ([UIDevice currentDeviceType] < DeviceTypeiPhone640x1136) {
-        for (NSLayoutConstraint *constraint in self.btnViewHeights) {
-            constraint.constant = AUTOLAYOUT_LENGTH(100);
-        }
-        for (NSLayoutConstraint *constraint in self.btnViewTops) {
-            constraint.constant = AUTOLAYOUT_LENGTH(10);
-        }
-        self.displayViewHeight.constant = AUTOLAYOUT_LENGTH(300);
-    }
+    
+    self.btnViewHeight.constant = kPadHeight;
+    
+    self.view.backgroundColor = RGB(188, 200, 173);
 }
 
 - (IBAction)digitPressed:(UIButton *)sender {
@@ -49,21 +60,40 @@
         self.display.text = @"";
     }
     self.isNew = NO;
+    
+    
+    NSString *text = [NSString stringWithFormat:@"%zd",sender.tag];
     if ([@"0" isEqualToString:self.display.text]) {
-        self.display.text = sender.currentTitle;
+        self.display.text = text;
     }
     else {
-        self.display.text = [self.display.text stringByAppendingString:sender.currentTitle];
+        self.display.text = [self.display.text stringByAppendingString:text];
     }
 }
+    
 - (IBAction)operationPressed:(UIButton *)sender {
-    NSString *operation = sender.currentTitle;
-    if ([@"÷" isEqualToString:operation]) {
+    /*
+     tag
+     
+     0 /
+     1 *
+     2 +
+     3 -
+     
+     */
+    NSString *operation = nil;
+    if (sender.tag==0) {
         operation = @"/";
-    }
-    else if ([@"x" isEqualToString:operation]) {
+    }else if (sender.tag==1) {
         operation = @"*";
     }
+    else if (sender.tag==2) {
+        operation = @"+";
+    }
+    else if (sender.tag==3) {
+        operation = @"-";
+    }
+    
     if (self.isNew) {
         self.display.text = @"";
     }
@@ -75,10 +105,13 @@
         self.display.text = [self.display.text stringByAppendingString:operation];
     }
 }
+    
 - (IBAction)zeroPressed {
     self.isNew = YES;
     self.display.text = @"0";
 }
+    
+    
 - (IBAction)deletePressed:(id)sender {
     if (isNotEmpty(self.display.text) && ! [@"0" isEqualToString:self.display.text]) {
         self.display.text = [NSString removeLastCharOfString:self.display.text];
@@ -87,6 +120,8 @@
         self.display.text = @"0";
     }
 }
+    
+    
 - (IBAction)equalR {
     self.isNew = YES;
     self.display.text = [NSString stringWithFormat:@"%@", [CaculatorUtility calcComplexFormulaString:self.display.text]];
